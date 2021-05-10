4d27f318c154008a55550dd550269431f420e4ff
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-10 11:18:20 -0500

    add html generation

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+const profileDataArgs = process.argv.slice(2);
+const [userName, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(userName, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
