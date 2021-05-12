b75c011ff2d8a78a1920f0e90869644129f617cc
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-12 17:14:17 -0500

    Completed project and added styles

--- src/page-template.js
+++ src/page-template.js
@@ -29,7 +29,7 @@ const generateProjects = projectsArr => {
               ${languages.join(', ')}
             </h5>
             <p>${description}</p>
-            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            <a href="${link}" class="btn" target='blank'><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
       })
@@ -46,7 +46,7 @@ const generateProjects = projectsArr => {
               ${languages.join(', ')}
             </h5>
             <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            <a href="${link}" class="btn mt-auto" target='blank'><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
       })
